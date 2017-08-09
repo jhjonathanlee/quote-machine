7b2755866884ac38efb956b5ac0d58f38180ab0a
Jonathan Lee <jh.jonathanlee@gmail.com>
2017-08-09 17:28:22 -0400

    added some quotes

---
+++ resources/quotes.js
@@ -0,0 +1,50 @@
+{
+    {
+        "author": "Ron Swanson",
+        "quote": "Put some alcohol in your mouth, to block the words from coming out"
+    },
+    {
+        "author": "Ron Swanson",
+        "quote": "Any dog under fifty pounds is a cat and cats are useless"
+    },
+    {
+        "author": "Ron Swanson",
+        "quote": "I've never known what bangs are, and I don't intend to learn"
+    },
+    {
+        "author": "Ron Swanson",
+        "quote": "Never half-ass two things. Whole-ass one thing"
+    },
+    {
+        "author": "Ron Swanson",
+        "quote": "I've had the same will since I was eight years old"
+    },
+    {
+        "author": "Ron Swanson",
+        "quote": "You had me at \"meat tornado\""
+    },
+    {
+        "author": "Ron Swanson",
+        "quote": "Clear aloohols are for rich women on diets"
+    },
+    {
+        "author": "Ron Swanson",
+        "quote": "There will be alcohol there, so I will go as well"
+    },
+    {
+        "author": "Ron Swanson",
+        "quote": "There's only one thing I hate more thanlying: skim milk. Which is water that's lying about being milk"
+    },
+    {
+        "author": "Ron Swanson",
+        "quote": "I know what I'm about, son"
+    },
+    {
+        "author": "Ron Swanson",
+        "quote": "I once worked with a guy for three years and never learned his name. Best friend I ever had"
+    },
+    {
+        "author": "Ron Swanson",
+        "quote": "Dear frozen yogurt, you are the celery of desserts. Be ice cream, or be nothing"
+    }
+}
