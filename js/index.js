4f90fa4b7bb103f13ad5d598606158564b7d60f3
Jonathan Lee <jh.jonathanlee@gmail.com>
2017-08-10 18:36:39 -0400

    basic random quote generating functionality working

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+function getNewQuote() {
+    $.ajax({
+        cache: false,
+        url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
+        dataType: "json",
+        success: function(json) {
+            $(".quote").html(json[0].content + "<p>" + json[0].title + "</p>");
+        },
+        error: function(xhr, status, error) {
+            console.log(error);
+        }
+    });
+}
+
 $(document).ready(function() {
-    $('.quote').html('a quote');
+    getNewQuote();
+    $("#btn-quote").on("click", function() {
+        getNewQuote();
+    });
+
 });
