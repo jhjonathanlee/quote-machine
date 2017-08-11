df222ea735e66b0e66cdf93aa9ce5b54af32921a
Jonathan Lee <jh.jonathanlee@gmail.com>
2017-08-11 17:34:41 -0400

    change colors, fixing margins

--- js/index.js
+++ js/index.js
@@ -1,15 +1,12 @@
 function getNewQuote() {
     $.ajax({
         cache: false,
-        url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
+        url: "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?",
         dataType: "json",
         success: function(json) {
-            var content = json[0].content;
-            $("#quote").html(content + "<p>- " + json[0].title + "</p>");
-            $("#btn-tweet").remove();
-            $("#span-tweet").append('<a href="https://twitter.com/intent/tweet?text='
-                + content.slice(3, content.length - 7).replace(/\s+/g, "&nbsp;") 
-                + '" class="btn btn-default btn-primary" id="btn-tweet"><i class="fa fa-twitter" aria-hidden="true"></i></a>');
+            $("#quote").text(json.quoteText);
+            $("#author").text(json.quoteAuthor);
+            $("#btn-tweet").attr("href", "https://twitter.com/intent/tweet?text=" + encodeURIComponent(json.quoteText));
         },
         error: function(xhr, status, error) {
             console.log(error);
