ac1ec53b1698de619c0df27f36e3e97035eaf43a
Jonathan Lee <jh.jonathanlee@gmail.com>
2017-08-10 23:59:49 -0400

    can now tweet out the quote

--- js/index.js
+++ js/index.js
@@ -4,7 +4,12 @@ function getNewQuote() {
         url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
         dataType: "json",
         success: function(json) {
-            $(".quote").html(json[0].content + "<p>" + json[0].title + "</p>");
+            var content = json[0].content;
+            $("#quote").html(content + "<p>- " + json[0].title + "</p>");
+            $("#btn-tweet").remove();
+            $("#span-tweet").append('<a href="https://twitter.com/intent/tweet?text='
+                + content.slice(3, content.length - 7).replace(/\s+/g, "&nbsp;") 
+                + '" class="btn btn-default btn-primary" id="btn-tweet"><i class="fa fa-twitter" aria-hidden="true"></i></a>');
         },
         error: function(xhr, status, error) {
             console.log(error);
